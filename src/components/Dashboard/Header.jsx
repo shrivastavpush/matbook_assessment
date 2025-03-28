0ae564eb25fe65080b8111f454c84883ef2aa3d8
Pushpendra Shrivastav <shrivastavsumit15@gmail.com>
2025-03-28 08:58:04 +0530

    updated hamburger image

--- src/components/Dashboard/Header.jsx
+++ src/components/Dashboard/Header.jsx
@@ -1,20 +1,24 @@
 import React, { useState } from "react";
-import { FaBars } from "react-icons/fa";
 import Sidebar from "./SideBar";
+import HamburgerDefault from '../../assets/hamburger-default.svg'
+import HamburgerActive from '../../assets/hamburger-active.svg'
 
 const Header = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isHovered, setIsHovered] = useState(false);
 
     return (
         <>
             <div className="flex items-center p-4">
-                <button
-                    className={`flex items-center justify-center w-10 h-10 border rounded-lg shadow cursor-pointer ${isSidebarOpen ? "border-red-500 bg-red-50" : "border-gray-300 bg-white"
-                        }`}
+
+                <img
+                    src={isHovered ? HamburgerActive : HamburgerDefault}
+                    alt="hamburger"
                     onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-                >
-                    <FaBars className={`${isSidebarOpen ? "text-red-500" : "text-gray-300"} text-xl`} />
-                </button>
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                    className="cursor-pointer" />
+
                 <h1 className="text-2xl font-bold ml-6">Workflow Builder</h1>
             </div>
 
